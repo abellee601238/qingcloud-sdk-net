75ff921229766364fe447e6a19c3c3e3ca2b9230
abellee <abellee@yunify.com>
2017-08-15 11:14:00 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QingCloudSDK.config;
+using QingCloudSDK.service;
+using System.IO;
+using System.Net;
+using QingCloudSDK.utils;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace QingCloudSDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Vxnets vxnets = new Vxnets(config, "pekdemo1");
+            Vxnets.JoinVxnetInput vxnetinput = new Vxnets.JoinVxnetInput();
+            vxnetinput.setVxnet("vxnet-8vqzj0h");
+            vxnetinput.setInstances(new String[] { "i-qn1dyq27" });
+            Vxnets.JoinVxnetOutput vxnetoutput = new Vxnets.JoinVxnetOutput();
+            vxnetoutput = vxnets.JoinVxnets(vxnetinput);
+            Console.WriteLine(vxnetoutput.getRetCode());
+            Console.ReadLine();
+        }
+        /*
+        //DeleteImagesInput Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Image image = new Image(config, "pekdemo1");//zone名称
+            Image.DeleteImagesInput input = new Image.DeleteImagesInput();
+            input.setZone("pekdemo1");
+            input.setImages(new string[] { "img-boqkzmse" });
+
+            Image.DeleteImagesOutput output = new Image.DeleteImagesOutput();
+
+            output = image.DeleteImages(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+
+            // Print the job ID.
+            Console.WriteLine(output.getJobId());
+            Console.ReadLine();
+        }
+        */
+        /*
+        //CaptureInstance Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Image image = new Image(config, "pekdemo1");//zone名称
+            Image.CaptureInstanceInput input = new Image.CaptureInstanceInput();
+            input.setZone("pekdemo1");
+            input.setImageName("test_centos7x64");
+            input.setInstance("i-x96t7kfg");
+
+            Image.CaptureInstanceOutput output = new Image.CaptureInstanceOutput();
+
+            output = image.CaptureInstance(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+
+            // Print the job ID.
+            Console.WriteLine(output.getJobId());
+            Console.ReadLine();
+        }*/
+
+        /*
+        //DescribeImages Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Image image = new Image(config, "pekdemo1");//zone名称
+            Image.DescribeImagesInput input = new Image.DescribeImagesInput();
+            input.setZone("pekdemo1");
+            input.setImages(new string[] { "img-boqkzmse" });
+
+            Image.DescribeImagesOutput output = new Image.DescribeImagesOutput();
+
+            output = image.DescribeImages(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+
+            // Print the job ID.
+            Console.WriteLine(output.getImageSet()[0]["status"]);
+            Console.WriteLine(output.getImageSet()[0]["features_supported"]);
+            Console.ReadLine();
+        }
+        */
+        /*
+        //DescribeInstances Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Instance intance = new Instance(config, "pekdemo1");//zone名称
+            Instance.DescribeInstancesInput input = new Instance.DescribeInstancesInput();
+            //input.setInstances(new string[] { "i-2o2sq9zw" });
+            Instance.DescribeInstancesOutput output = new Instance.DescribeInstancesOutput();
+
+            output = intance.DescribeInstances(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+            Console.WriteLine(output.getInstanceSet()[0]["status"]);
+            Console.WriteLine(output.getInstanceSet()[0]["vxnets"][0]["role"]);
+            Console.ReadLine();
+        }
+        */
+
+        /*
+        //RunInstances Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Instance intance = new Instance(config, "pekdemo1");//zone名称
+            Instance.RunInstancesInput input = new Instance.RunInstancesInput();
+            String[] volumes = { "vol-3yleq25h" };
+            String[] networks = { "vxnet-0" };
+            input.setImageId("img-gp3zihbr");                      //这个参数是我们自己抽取出来的，调试时候需要用现成的常量来代替
+            input.setCpu(1);
+            input.setMemory(1024);
+            input.setInstanceName("test_vm_x");                            //这个参数是我们自己抽取出来的，调试时候需要用现成的常量来代替
+            //input.setVolumes(volumes);                      //这个参数是我们自己抽取出来的，调试时候需要用现成的常量来代替
+            input.setLoginMode("passwd");
+            input.setLoginPassword("Zhu88jie");
+            input.setVxnets(networks);                       //这个参数是我们自己抽取出来的，调试时候需要用现成的常量来代替
+            input.setZone("pekdemo1");
+            input.setCount(2);
+            //String[] vxnets  paul:where to ensue the network in catalog? should use the one in profilesetting or creation request?  
+
+            Instance.RunInstancesOutput output = new Instance.RunInstancesOutput();
+
+            output = intance.RunInstances(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+            Console.WriteLine(output.getInstances()[0]);
+            Console.WriteLine(output.getInstances()[1]);
+            // Print the job ID.
+            Console.WriteLine(output.getJobId());
+
+            Console.ReadLine();
+        }
+        */
+        /*
+         //StartInstances Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Instance intance = new Instance(config, "pekdemo1");//zone名称
+            Instance.StartInstancesInput input = new Instance.StartInstancesInput();
+            input.setInstances(new String[] { "i-tj3e7xpl", "i-71x8sg3x" });
+            Instance.StartInstancesOutput output = new Instance.StartInstancesOutput();
+            output = intance.StartInstances(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+
+            // Print the job ID.
+            Console.WriteLine(output.getJobId());
+            Console.ReadLine();
+        }
+         * */
+
+        /*
+         //StopInstances Demo
+        static void Main(string[] args)
+        {
+            Config config = new Config("KLVGRRVCZISPWZWUTTNK", "fWvtysXGLcc3ATXRWwuIVjJ6RRIv3hYtqyF3zgNx");//citrix@citrix.com的key和secret
+            config.setProtocol("http");
+            config.setHost("api.pekdemo.com");
+            config.setPort("80");
+            Instance intance = new Instance(config, "pekdemo1");//zone名称
+            Instance.StopInstancesInput input = new Instance.StopInstancesInput();
+            input.setInstances(new String[] { "i-tj3e7xpl", "i-71x8sg3x" });
+            Instance.StopInstancesOutput output = new Instance.StopInstancesOutput();
+            output = intance.StopInstances(input);
+
+            // Print the return code.
+            Console.WriteLine(output.getRetCode());
+
+            // Print the job ID.
+            Console.WriteLine(output.getJobId());
+            Console.ReadLine();
+        }
+         * */
+    }
+}
---
+++ src/Service/Image.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+using QingCloudSDK.service;
+using QingCloudSDK.Attribute;
+using QingCloudSDK.config;
+using QingCloudSDK.constants;
+using QingCloudSDK.exception;
+using QingCloudSDK.model;
+using QingCloudSDK.request;
+using QingCloudSDK.utils;
+
+namespace QingCloudSDK.service
+{
+    public class Image
+    {
+        private String zone;
+        private Config Config;
+
+        public Image(Config Config) 
+        {
+            this.Config = Config;
+            this.zone = Constant.CLOUD_DEFAULT_ZONE;
+        }
+
+        public Image(Config Config, String zone) 
+        {
+            this.Config = Config;
+            this.zone = zone;
+        }
+
+        public class DescribeImagesInput : RequestInputModel
+        {
+            private String []images;
+            [Param(paramType = "header", paramName = "images")]
+            public String []getImages()
+            {
+                return this.images;
+            }
+            public void setImages(String[] images)
+            {
+                this.images = images;
+            }
+
+            private String processorType;
+            [Param(paramType = "header", paramName = "processor_type")]
+            public String getProcessorType()
+            {
+                return this.processorType;
+            }
+            public void setProcessorType(String processorType)
+            {
+                this.processorType = processorType;
+            }
+
+            private String osFamily;
+            [Param(paramType = "header", paramName = "os_family")]
+            public String getOsFamily()
+            {
+                return this.osFamily;
+            }
+            public void setOsFamily(String osFamily)
+            {
+                this.osFamily = osFamily;
+            }
+
+            private String visibility;
+            [Param(paramType = "header", paramName = "visibility")]
+            public String getVisibility()
+            {
+                return this.visibility;
+            }
+            public void setVisibility(String visibility)
+            {
+                this.visibility = visibility;
+            }
+
+            private String provider;
+            [Param(paramType = "header", paramName = "provider")]
+            public String getProvider()
+            {
+                return this.provider;
+            }
+            public void setProvider(String provider)
+            {
+                this.provider = provider;
+            }
+
+            private String[] status;
+            [Param(paramType = "header", paramName = "status")]
+            public String[] getStatus()
+            {
+                return this.status;
+            }
+            public void setStatus(String[] status)
+            {
+                this.status = status;
+            }
+
+            private String searchWord;
+            [Param(paramType = "header", paramName = "search_word")]
+            public String getSearchWord()
+            {
+                return this.searchWord;
+            }
+            public void setSearchWord(String searchWord)
+            {
+                this.searchWord = searchWord;
+            }
+
+            private int verbose;
+            [Param(paramType = "header", paramName = "verbose")]
+            public int getVerbose()
+            {
+                return this.verbose;
+            }
+            public void setVerbose(int verbose)
+            {
+                this.verbose = verbose;
+            }
+
+            private int offset;
+            [Param(paramType = "header", paramName = "offset")]
+            public int getOffset()
+            {
+                return this.offset;
+            }
+            public void setOffset(int offset)
+            {
+                this.offset = offset;
+            }
+
+            private int limit;
+            [Param(paramType = "header", paramName = "limit")]
+            public int getLimit()
+            {
+                return this.limit;
+            }
+            public void setLimit(int limit)
+            {
+                this.limit = limit;
+            }
+
+            private String zone;
+            [Param(paramType = "header", paramName = "zone")]
+            public String getZone()
+            {
+                return this.zone;
+            }
+            public void setZone(String zone)
+            {
+                this.zone = zone;
+            }
+
+            public override String validateParam()
+            {
+
+                return null;
+            }
+        }
+
+        public class DescribeImagesOutput : OutputModel
+        {
+            private String action;
+            [Param(paramType = "query", paramName = "action")]
+            public String getAction()
+            {
+                return this.action;
+            }
+            public void setAction(String action)
+            {
+                this.action = action;
+            }
+
+            private String imageSet;
+            [Param(paramType = "query", paramName = "image_set")]
+            public JArray getImageSet()
+            {
+                return JArray.Parse(this.imageSet);
+            }
+            public void setImageSet(String imageSet)
+            {
+                this.imageSet = imageSet;
+            }
+
+            private int totalCount;
+            [Param(paramType = "query", paramName = "total_count")]
+            public int getTotalCount()
+            {
+                return this.totalCount;
+            }
+            public void setTotalCount(int totalCount)
+            {
+                this.totalCount = totalCount;
+            }
+
+            private int retCode;
+            [Param(paramType = "query", paramName = "ret_code")]
+            public int getRetCode()
+            {
+                return this.retCode;
+            }
+            public void setRetCode(int retCode)
+            {
+                this.retCode = retCode;
+            }
+        }
+
+        public DescribeImagesOutput DescribeImages(DescribeImagesInput input)
+        {
+            Dictionary<object, object> context = new Dictionary<object, object>();
+            context.Add(Constant.PARAM_KEY_REQUEST_ZONE, this.zone);
+            context.Add(Constant.CONFIG_CONTEXT_KEY, this.Config);
+            context.Add("action", "DescribeImages");
+            context.Add("APIName", "DescribeImages");
+            context.Add("ServiceName", "Describe Images");
+            context.Add("RequestMethod", "GET");
+            //context.Add("RequestURI", "/<>");
+            //context.Add("instanceNameInput", this.instanceName);
+
+            if (StringUtil.isEmpty(zone))
+            {
+                throw new QCException("zone can't be empty!");
+            }
+
+            OutputModel backModel =
+                    ResourceRequestFactory.getResourceRequest()
+                            .sendApiRequest(context, input, typeof(DescribeImagesOutput));
+            if (backModel != null)
+            {
+                return (DescribeImagesOutput)backModel;
+            }
+            return null;
+        }
+
+        public class CaptureInstanceInput: RequestInputModel
+        {
+            private String instance;
+            [Param(paramType = "header", paramName = "instance")]
+            public String getInstance()
+            {
+                return this.instance;
+            }
+            public void setInstance(String instance)
+            {
+                this.instance = instance;
+            }
+
+            private String imageName;
+            [Param(paramType = "header", paramName = "image_name")]
+            public String getImageName()
+            {
+                return this.imageName;
+            }
+            public void setImageName(String imageName)
+            {
+                this.imageName = imageName;
+            }
+
+            private String targetUser;
+            [Param(paramType = "header", paramName = "target_user")]
+            public String getTargetUser()
+            {
+                return this.targetUser;
+            }
+            public void setTargetUser(String targetUser)
+            {
+                this.targetUser = targetUser;
+            }
+
+            private String zone;
+            [Param(paramType = "header", paramName = "zone")]
+            public String getZone()
+            {
+                return this.zone;
+            }
+            public void setZone(String zone)
+            {
+                this.zone = zone;
+            }
+
+            public override String validateParam()
+            {
+                return null;
+            }
+        }
+
+        public class CaptureInstanceOutput : OutputModel
+        {
+            private String action;
+            [Param(paramType = "query", paramName = "action")]
+            public String getAction()
+            {
+                return this.action;
+            }
+            public void setAction(String action)
+            {
+                this.action = action;
+            }
+
+            private String imageId;
+            [Param(paramType = "query", paramName = "image_id")]
+            public String getImageId()
+            {
+                return this.imageId;
+            }
+            public void setImageId(String imageId)
+            {
+                this.imageId = imageId;
+            }
+
+            private String jobId;
+            [Param(paramType = "query", paramName = "job_id")]
+            public String getJobId()
+            {
+                return this.jobId;
+            }
+            public void setJobId(String jobId)
+            {
+                this.jobId = jobId;
+            }
+
+            private int retCode;
+            [Param(paramType = "query", paramName = "ret_code")]
+            public int getRetCode()
+            {
+                return this.retCode;
+            }
+            public void setRetCode(int retCode)
+            {
+                this.retCode = retCode;
+            }
+        }
+
+        public CaptureInstanceOutput CaptureInstance(CaptureInstanceInput input)
+        {
+            Dictionary<object, object> context = new Dictionary<object, object>();
+            context.Add(Constant.PARAM_KEY_REQUEST_ZONE, this.zone);
+            context.Add(Constant.CONFIG_CONTEXT_KEY, this.Config);
+            context.Add("action", "CaptureInstance");
+            context.Add("APIName", "CaptureInstance");
+            context.Add("ServiceName", "Capture Instance");
+            context.Add("RequestMethod", "GET");
+
+            if (StringUtil.isEmpty(zone))
+            {
+                throw new QCException("zone can't be empty!");
+            }
+
+            OutputModel backModel =
+                    ResourceRequestFactory.getResourceRequest()
+                            .sendApiRequest(context, input, typeof(CaptureInstanceOutput));
+            if (backModel != null)
+            {
+                return (CaptureInstanceOutput)backModel;
+            }
+            return null;
+        }
+
+        public class DeleteImagesInput : RequestInputModel
+        {
+            private String[] images;
+            [Param(paramType = "header", paramName = "images")]
+            public String[] getImages()
+            {
+                return this.images;
+            }
+            public void setImages(String[] images)
+            {
+                this.images = images;
+            }
+
+            private String zone;
+            [Param(paramType = "header", paramName = "zone")]
+            public String getZone()
+            {
+                return this.zone;
+            }
+            public void setZone(String zone)
+            {
+                this.zone = zone;
+            }
+
+            public override String validateParam()
+            {
+                return null;
+            }
+        }
+
+        public class DeleteImagesOutput : OutputModel
+        {
+            private String action;
+            [Param(paramType = "query", paramName = "action")]
+            public String getAction()
+            {
+                return this.action;
+            }
+            public void setAction(String action)
+            {
+                this.action = action;
+            }
+
+            private String jobId;
+            [Param(paramType = "query", paramName = "job_id")]
+            public String getJobId()
+            {
+                return this.jobId;
+            }
+            public void setJobId(String jobId)
+            {
+                this.jobId = jobId;
+            }
+
+            private int retCode;
+            [Param(paramType = "query", paramName = "ret_code")]
+            public int getRetCode()
+            {
+                return this.retCode;
+            }
+            public void setRetCode(int retCode)
+            {
+                this.retCode = retCode;
+            }
+        }
+
+        public DeleteImagesOutput DeleteImages(DeleteImagesInput input)
+        {
+            Dictionary<object, object> context = new Dictionary<object, object>();
+            context.Add(Constant.PARAM_KEY_REQUEST_ZONE, this.zone);
+            context.Add(Constant.CONFIG_CONTEXT_KEY, this.Config);
+            context.Add("action", "DeleteImages");
+            context.Add("APIName", "DeleteImages");
+            context.Add("ServiceName", "Delete Images");
+            context.Add("RequestMethod", "GET");
+
+            if (StringUtil.isEmpty(zone))
+            {
+                throw new QCException("zone can't be empty!");
+            }
+
+            OutputModel backModel =
+                    ResourceRequestFactory.getResourceRequest()
+                            .sendApiRequest(context, input, typeof(DeleteImagesOutput));
+            if (backModel != null)
+            {
+                return (DeleteImagesOutput)backModel;
+            }
+            return null;
+        }
+    }
+}
